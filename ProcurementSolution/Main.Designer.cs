d2a9124734c4c2f2154c5898379071cfa32713c4
Adnan Muhib <adnan.muhib@rocketmail.com>
2018-02-17 15:05:19 +0500

    :sparkle: ADD: Main Form Layout Sketch

---
+++ ProcurementSolution/Main.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ProcurementSolution
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.AccessoriesPage = new System.Windows.Forms.TabPage();
+            this.EmployeesPage = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.IssueAccPage = new System.Windows.Forms.TabPage();
+            this.AvailableAccPage = new System.Windows.Forms.TabPage();
+            this.AccCategoryPage = new System.Windows.Forms.TabPage();
+            this.AccAddStockPage = new System.Windows.Forms.TabPage();
+            this.tabControl3 = new System.Windows.Forms.TabControl();
+            this.NewEmpPage = new System.Windows.Forms.TabPage();
+            this.AllEmpPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.AccessoriesPage.SuspendLayout();
+            this.EmployeesPage.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabControl3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.AccessoriesPage);
+            this.tabControl1.Controls.Add(this.EmployeesPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(625, 493);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // AccessoriesPage
+            // 
+            this.AccessoriesPage.Controls.Add(this.tabControl2);
+            this.AccessoriesPage.Location = new System.Drawing.Point(4, 27);
+            this.AccessoriesPage.Name = "AccessoriesPage";
+            this.AccessoriesPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AccessoriesPage.Size = new System.Drawing.Size(617, 462);
+            this.AccessoriesPage.TabIndex = 0;
+            this.AccessoriesPage.Text = "Accessories";
+            this.AccessoriesPage.UseVisualStyleBackColor = true;
+            // 
+            // EmployeesPage
+            // 
+            this.EmployeesPage.Controls.Add(this.tabControl3);
+            this.EmployeesPage.Location = new System.Drawing.Point(4, 27);
+            this.EmployeesPage.Name = "EmployeesPage";
+            this.EmployeesPage.Padding = new System.Windows.Forms.Padding(3);
+            this.EmployeesPage.Size = new System.Drawing.Size(617, 462);
+            this.EmployeesPage.TabIndex = 1;
+            this.EmployeesPage.Text = "Employees";
+            this.EmployeesPage.UseVisualStyleBackColor = true;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.IssueAccPage);
+            this.tabControl2.Controls.Add(this.AvailableAccPage);
+            this.tabControl2.Controls.Add(this.AccAddStockPage);
+            this.tabControl2.Controls.Add(this.AccCategoryPage);
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl2.Location = new System.Drawing.Point(3, 3);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(611, 456);
+            this.tabControl2.TabIndex = 0;
+            // 
+            // IssueAccPage
+            // 
+            this.IssueAccPage.Location = new System.Drawing.Point(4, 27);
+            this.IssueAccPage.Name = "IssueAccPage";
+            this.IssueAccPage.Padding = new System.Windows.Forms.Padding(3);
+            this.IssueAccPage.Size = new System.Drawing.Size(603, 425);
+            this.IssueAccPage.TabIndex = 0;
+            this.IssueAccPage.Text = "Issue Accessory";
+            this.IssueAccPage.UseVisualStyleBackColor = true;
+            // 
+            // AvailableAccPage
+            // 
+            this.AvailableAccPage.Location = new System.Drawing.Point(4, 27);
+            this.AvailableAccPage.Name = "AvailableAccPage";
+            this.AvailableAccPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AvailableAccPage.Size = new System.Drawing.Size(579, 384);
+            this.AvailableAccPage.TabIndex = 1;
+            this.AvailableAccPage.Text = "Available Accessories";
+            this.AvailableAccPage.UseVisualStyleBackColor = true;
+            // 
+            // AccCategoryPage
+            // 
+            this.AccCategoryPage.Location = new System.Drawing.Point(4, 33);
+            this.AccCategoryPage.Name = "AccCategoryPage";
+            this.AccCategoryPage.Size = new System.Drawing.Size(475, 378);
+            this.AccCategoryPage.TabIndex = 2;
+            this.AccCategoryPage.Text = "Accessory Category";
+            this.AccCategoryPage.UseVisualStyleBackColor = true;
+            // 
+            // AccAddStockPage
+            // 
+            this.AccAddStockPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.AccAddStockPage.Location = new System.Drawing.Point(4, 27);
+            this.AccAddStockPage.Name = "AccAddStockPage";
+            this.AccAddStockPage.Size = new System.Drawing.Size(579, 384);
+            this.AccAddStockPage.TabIndex = 3;
+            this.AccAddStockPage.Text = "New Accessory Stock";
+            this.AccAddStockPage.UseVisualStyleBackColor = true;
+            // 
+            // tabControl3
+            // 
+            this.tabControl3.Controls.Add(this.NewEmpPage);
+            this.tabControl3.Controls.Add(this.AllEmpPage);
+            this.tabControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl3.Location = new System.Drawing.Point(3, 3);
+            this.tabControl3.Name = "tabControl3";
+            this.tabControl3.SelectedIndex = 0;
+            this.tabControl3.Size = new System.Drawing.Size(611, 456);
+            this.tabControl3.TabIndex = 0;
+            // 
+            // NewEmpPage
+            // 
+            this.NewEmpPage.Location = new System.Drawing.Point(4, 27);
+            this.NewEmpPage.Name = "NewEmpPage";
+            this.NewEmpPage.Padding = new System.Windows.Forms.Padding(3);
+            this.NewEmpPage.Size = new System.Drawing.Size(579, 401);
+            this.NewEmpPage.TabIndex = 0;
+            this.NewEmpPage.Text = "New Employee";
+            this.NewEmpPage.UseVisualStyleBackColor = true;
+            // 
+            // AllEmpPage
+            // 
+            this.AllEmpPage.Location = new System.Drawing.Point(4, 27);
+            this.AllEmpPage.Name = "AllEmpPage";
+            this.AllEmpPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AllEmpPage.Size = new System.Drawing.Size(603, 425);
+            this.AllEmpPage.TabIndex = 1;
+            this.AllEmpPage.Text = "All Employees";
+            this.AllEmpPage.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(625, 493);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UET Procurement Solution";
+            this.tabControl1.ResumeLayout(false);
+            this.AccessoriesPage.ResumeLayout(false);
+            this.EmployeesPage.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.tabControl3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage AccessoriesPage;
+        private System.Windows.Forms.TabPage EmployeesPage;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage IssueAccPage;
+        private System.Windows.Forms.TabPage AvailableAccPage;
+        private System.Windows.Forms.TabPage AccAddStockPage;
+        private System.Windows.Forms.TabPage AccCategoryPage;
+        private System.Windows.Forms.TabControl tabControl3;
+        private System.Windows.Forms.TabPage NewEmpPage;
+        private System.Windows.Forms.TabPage AllEmpPage;
+    }
+}
+
--- ProcurementSolution/Program.cs
+++ ProcurementSolution/Program.cs
@@ -16,7 +16,7 @@ namespace ProcurementSolution
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
